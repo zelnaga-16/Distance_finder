b450137c099f29cca06b4882605b71a0038f78c5
jariz <jarizleifr06@gmail.com>
2023-08-17 16:54:14 +0200

    second version

--- Distance_finder/Form1.Designer.cs
+++ Distance_finder/Form1.Designer.cs
@@ -34,6 +34,8 @@
             Button = new Button();
             label2 = new Label();
             result_lable = new Label();
+            gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            gMapControl2 = new GMap.NET.WindowsForms.GMapControl();
             SuspendLayout();
             // 
             // address_1
@@ -41,7 +43,7 @@
             address_1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             address_1.Location = new Point(479, 31);
             address_1.Name = "address_1";
-            address_1.Size = new Size(522, 47);
+            address_1.Size = new Size(1465, 47);
             address_1.TabIndex = 0;
             // 
             // address_2
@@ -49,7 +51,7 @@
             address_2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             address_2.Location = new Point(479, 146);
             address_2.Name = "address_2";
-            address_2.Size = new Size(522, 47);
+            address_2.Size = new Size(1465, 47);
             address_2.TabIndex = 1;
             // 
             // label1
@@ -59,18 +61,18 @@
             label1.ForeColor = Color.Black;
             label1.Location = new Point(227, 31);
             label1.Name = "label1";
-            label1.Size = new Size(166, 41);
+            label1.Size = new Size(184, 41);
             label1.TabIndex = 2;
-            label1.Text = "First adress";
+            label1.Text = "First address";
             label1.Click += label1_Click;
             // 
             // Button
             // 
             Button.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             Button.ForeColor = Color.Black;
-            Button.Location = new Point(364, 373);
+            Button.Location = new Point(650, 909);
             Button.Name = "Button";
-            Button.Size = new Size(425, 173);
+            Button.Size = new Size(856, 175);
             Button.TabIndex = 4;
             Button.Text = "Find distace";
             Button.UseVisualStyleBackColor = true;
@@ -83,9 +85,9 @@
             label2.ForeColor = Color.Black;
             label2.Location = new Point(183, 146);
             label2.Name = "label2";
-            label2.Size = new Size(210, 41);
+            label2.Size = new Size(228, 41);
             label2.TabIndex = 6;
-            label2.Text = "Second adress";
+            label2.Text = "Second address";
             label2.Click += label2_Click;
             // 
             // result_lable
@@ -93,18 +95,76 @@
             result_lable.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             result_lable.AutoSize = true;
             result_lable.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
-            result_lable.Location = new Point(292, 245);
+            result_lable.Location = new Point(798, 230);
             result_lable.Name = "result_lable";
             result_lable.Size = new Size(0, 62);
             result_lable.TabIndex = 7;
             result_lable.Click += label3_Click_1;
+            // 
+            // gMapControl1
+            // 
+            gMapControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            gMapControl1.Bearing = 0F;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.EmptyTileColor = Color.Navy;
+            gMapControl1.GrayScaleMode = false;
+            gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            gMapControl1.LevelsKeepInMemmory = 5;
+            gMapControl1.Location = new Point(34, 330);
+            gMapControl1.MarkersEnabled = true;
+            gMapControl1.MaxZoom = 2;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.MouseWheelZoomEnabled = true;
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl1.Name = "gMapControl1";
+            gMapControl1.NegativeMode = false;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.RetryLoadTile = 0;
+            gMapControl1.RoutesEnabled = true;
+            gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            gMapControl1.SelectedAreaFillColor = Color.FromArgb(33, 65, 105, 225);
+            gMapControl1.ShowTileGridLines = false;
+            gMapControl1.Size = new Size(950, 515);
+            gMapControl1.TabIndex = 8;
+            gMapControl1.Zoom = 0D;
+            gMapControl1.Load += gMapControl1_Load;
+            // 
+            // gMapControl2
+            // 
+            gMapControl2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            gMapControl2.Bearing = 0F;
+            gMapControl2.CanDragMap = true;
+            gMapControl2.EmptyTileColor = Color.Navy;
+            gMapControl2.GrayScaleMode = false;
+            gMapControl2.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            gMapControl2.LevelsKeepInMemmory = 5;
+            gMapControl2.Location = new Point(1173, 330);
+            gMapControl2.MarkersEnabled = true;
+            gMapControl2.MaxZoom = 2;
+            gMapControl2.MinZoom = 2;
+            gMapControl2.MouseWheelZoomEnabled = true;
+            gMapControl2.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl2.Name = "gMapControl2";
+            gMapControl2.NegativeMode = false;
+            gMapControl2.PolygonsEnabled = true;
+            gMapControl2.RetryLoadTile = 0;
+            gMapControl2.RoutesEnabled = true;
+            gMapControl2.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            gMapControl2.SelectedAreaFillColor = Color.FromArgb(33, 65, 105, 225);
+            gMapControl2.ShowTileGridLines = false;
+            gMapControl2.Size = new Size(950, 515);
+            gMapControl2.TabIndex = 9;
+            gMapControl2.Zoom = 0D;
+            gMapControl2.Load += gMapControl2_Load;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(1205, 749);
+            ClientSize = new Size(2148, 1151);
+            Controls.Add(gMapControl2);
+            Controls.Add(gMapControl1);
             Controls.Add(result_lable);
             Controls.Add(label2);
             Controls.Add(Button);
@@ -128,5 +188,7 @@
         private Button Button;
         private Label label2;
         private Label result_lable;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl2;
     }
 }
--- Distance_finder/Form1.cs
+++ Distance_finder/Form1.cs
@@ -1,6 +1,5 @@
-
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
 
 namespace Distance_finder
 {
@@ -76,8 +75,37 @@ namespace Distance_finder
             double latitude_2 = 0.0;
             double longitude_2 = 0.0;
             //Check result
+
             bool Check_1 = Convert.ToBoolean(Get_lat_and_long(address_1.Text, ref latitude_1, ref longitude_1));
+            if (Check_1)
+            {
+                gMapControl1.Position = new GMap.NET.PointLatLng(latitude_1, longitude_1);
+                gMapControl1.Zoom = 20;
+
+                List<Marker> markers_1 = new List<Marker>//marker on first map
+                {
+                    new Marker()
+                };
+                markers_1[0].Set(address_1.Text, latitude_1, longitude_1);//info for first map
+
+                gMapControl1.Overlays.Add(GetOverlayMarkers(markers_1, "First map"));//marker for first map
+                gMapControl1.Update();// update first control
+            }
+
             bool Check_2 = Convert.ToBoolean(Get_lat_and_long(address_2.Text, ref latitude_2, ref longitude_2));
+            if (Check_2)
+            {
+                gMapControl2.Position = new GMap.NET.PointLatLng(latitude_2, longitude_2);
+                gMapControl2.Zoom = 20;
+                List<Marker> markers_2 = new List<Marker>//marker on second map
+                {
+                    new Marker()
+                };
+                markers_2[0].Set(address_2.Text, latitude_2, longitude_2);//info for second map
+
+                gMapControl2.Overlays.Add(GetOverlayMarkers(markers_2, "Second map"));//marker for second map
+                gMapControl2.Update();// update second control
+            }
             //find distance
             if (Check_1 && Check_2)
             {
@@ -134,6 +162,75 @@ namespace Distance_finder
         private void label3_Click_1(object sender, EventArgs e)
         {
 
+        }
+        private GMapOverlay GetOverlayMarkers(List<Marker> markers, string name, GMarkerGoogleType gMarkerGoogleType = GMarkerGoogleType.red)
+        {
+            GMapOverlay gMapMarkers = new GMapOverlay(name);// creating a named layer
+            foreach (Marker marker in markers)
+            {
+                gMapMarkers.Markers.Add(GetMarker(marker, gMarkerGoogleType));// adding markers to a layer
+            }
+            return gMapMarkers;
+        }
+        private GMarkerGoogle GetMarker(Marker marker, GMarkerGoogleType gMarkerGoogleType = GMarkerGoogleType.red)
+        {
+            GMarkerGoogle mapMarker = new GMarkerGoogle(new GMap.NET.PointLatLng(marker.Get_latitude(), marker.Get_longitude()), gMarkerGoogleType);//latitude, longitude, marker type
+            mapMarker.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(mapMarker);//marker info popup
+            mapMarker.ToolTipText = marker.Get_text(); // text inside the popup
+            mapMarker.ToolTipMode = MarkerTooltipMode.OnMouseOver; //popup display (on hover)
+            return mapMarker;
+        }
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //choice of map loading - online or from resources
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; //which map provider is used (in our case, Google) 
+            gMapControl1.MinZoom = 2; //minimum zoom
+            gMapControl1.MaxZoom = 24; //maximum zoom
+            gMapControl1.Zoom = 4; // what zoom is used when opening
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // how it zooms in (just to the center of the map or by mouse position)
+            gMapControl1.CanDragMap = true; // dragging the map with the mouse
+            gMapControl1.DragButton = MouseButtons.Left; // which button is used for dragging
+            gMapControl1.ShowCenter = false; //show or hide the red cross in the center
+            gMapControl1.ShowTileGridLines = false; //show or hide tiles
+        }
+        private void gMapControl2_Load(object sender, EventArgs e)
+        {
+            // second map, like first 
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; 
+            gMapControl2.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; 
+            gMapControl2.MinZoom = 2; 
+            gMapControl2.MaxZoom = 24; 
+            gMapControl2.Zoom = 4; 
+            gMapControl2.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; 
+            gMapControl2.CanDragMap = true; 
+            gMapControl2.DragButton = MouseButtons.Left; 
+            gMapControl2.ShowCenter = false; 
+            gMapControl2.ShowTileGridLines = false; 
+        }
+    }
+    public class Marker// class with info for marker
+    {
+        string text;
+        double latitude;
+        double longitude;
+
+        public void Set(string text, double latitude, double longitude)
+        {
+            this.text = text;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+        public string Get_text()
+        {
+            return text;
+        }
+        public double Get_latitude()
+        {
+            return latitude;
+        }
+        public double Get_longitude()
+        {
+            return longitude;
         }
     }
 }
